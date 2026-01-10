f258b24d72a63869965bd93de15dc6cc37645c34
Marco <marcofratini@outlook.com>
2026-01-10 12:20:49 +0100

    add structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullscreenMapPage,
+    title: 'FullScreen Map',
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen',
+  },
+];
---
+++ src/app/pages/market-page/market-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-page',
+  imports: [],
+  templateUrl: './market-page.html',
+  styleUrl: './market-page.css',
+})
+export class MarketPage { }
